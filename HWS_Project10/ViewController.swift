60afddad39514750c65404c31df6fff123bc6053
Aleksey DreamLine <aleksey.kabishau@dreamline.com>
2019-05-18 14:07:59 -0400

    basic setup of custom collection view cell

--- HWS_Project10/ViewController.swift
+++ HWS_Project10/ViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        cell.nameLabel.text = "Aleksey"
+        return cell
     }
 }
 
